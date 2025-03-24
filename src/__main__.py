047a4af414902c6ee84beb49be067cb3a02edaa6
AshokShau <114943948+AshokShau@users.noreply.github.com>
2025-03-24 23:36:06 +0530

    add support to save cookies from BatBin urls

--- src/__main__.py
+++ src/__main__.py
@@ -6,9 +6,11 @@ from src import client
 
 async def create_directories() -> None:
     """Create necessary directories."""
+    from src.platforms.save_cookies import save_all_cookies
     try:
         await os.makedirs(config.DOWNLOADS_DIR, exist_ok=True)
-        await os.makedirs("database/photos", exist_ok=True)
+        await os.makedirs(f"{config.DOWNLOADS_DIR}/photos", exist_ok=True)
+        await save_all_cookies(config.COOKIES_URL)
     except Exception as e:
         raise SystemExit(1) from e
 
