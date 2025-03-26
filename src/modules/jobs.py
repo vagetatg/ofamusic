d1a68d107fa4791e961d852f34a0c5fe83dca16a
AshokShau <114943948+AshokShau@users.noreply.github.com>
2025-03-27 02:12:37 +0530

    Copyright

--- src/modules/jobs.py
+++ src/modules/jobs.py
@@ -1,3 +1,9 @@
+#  Copyright (c) 2025 AshokShau.
+#  TgMusicBot is an open-source Telegram music bot licensed under AGPL-3.0.
+#  All rights reserved where applicable.
+#
+#
+
 import asyncio
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
--- src/platforms/__init__.py
+++ src/platforms/__init__.py
@@ -1,5 +1,11 @@
 __all = ["JiosaavnData", "SpotifyData", "YouTubeData"]
 
+#  Copyright (c) 2025 AshokShau.
+#  TgMusicBot is an open-source Telegram music bot licensed under AGPL-3.0.
+#  All rights reserved where applicable.
+#
+#
+
 from ._jiosaavn import JiosaavnData
 from ._spotify import SpotifyData
 from ._youtube import YouTubeData
