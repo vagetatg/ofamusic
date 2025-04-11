fc3b21237c763560242dc23ad7aad942e8210ba4
AshokShau <114943948+AshokShau@users.noreply.github.com>
2025-04-11 13:31:04 +0530

    v1.1.4

--- src/__init__.py
+++ src/__init__.py
@@ -15,7 +15,7 @@ from src.database import db
 from src.modules.jobs import InactiveCallManager
 from src.pytgcalls import call, start_clients
 
-__version__ = "1.1.3"
+__version__ = "1.1.4"
 
 
 class Telegram(Client):
